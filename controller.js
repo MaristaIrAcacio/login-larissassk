1eef30e4ac43f40f5229948b1a051c88bb0b94f9
Larissa Manrique <larissasantos200702@gmail.com>
2023-06-27 08:26:18 -0300

    modificando arquivos

--- controller.js
+++ controller.js
@@ -1,9 +1,11 @@
+
 function acessar() {
     let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
+ 
     if(!loginEmail || !loginSenha) {
         alert("favor preencha os campos para acessar o site");
     } else {
-       alert("Campos preenchidos com sucesso");
+       window.location.href = "aula2.html";
     }
 }
